9e22e80f32f6aa85fa8515c38bfd2a0ac4bec782
MARCIN-PC\Marcin <nalkez69@gmail.com>
2021-05-26 14:44:27 +0200

    build

---
+++ build/precache-manifest.e65d5cc8d336cabef9dcf94844355412.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47737bd57219a1c9b3a69c1fd593db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e0ec0923672da01235",
+    "url": "/static/css/main.3c5fb90d.chunk.css"
+  },
+  {
+    "revision": "bbb045c82fe8362e3ac9",
+    "url": "/static/js/2.fe8297c3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.fe8297c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e0ec0923672da01235",
+    "url": "/static/js/main.c39098c9.chunk.js"
+  },
+  {
+    "revision": "31c709ef63031acc4454",
+    "url": "/static/js/runtime-main.1e9cc892.js"
+  },
+  {
+    "revision": "91bde36b5ff31fa96a1ddecd38c2ed32",
+    "url": "/static/media/GraphikLCG-Light.91bde36b.woff2"
+  },
+  {
+    "revision": "10b7bd9b829afb254fb3c2e56229ef8c",
+    "url": "/static/media/GraphikLCG-Medium.10b7bd9b.woff2"
+  },
+  {
+    "revision": "2f338b5ebd9e450c9e4cfcccb69100eb",
+    "url": "/static/media/GraphikLCG-Regular.2f338b5e.woff2"
+  },
+  {
+    "revision": "f644201ada7eca66a479068ca860bb3e",
+    "url": "/static/media/MB.f644201a.png"
+  },
+  {
+    "revision": "d0ae14336d4144126fcd99551e0bab9a",
+    "url": "/static/media/aboutme.d0ae1433.svg"
+  },
+  {
+    "revision": "052667b9d3aa6f1098a318883fe9ef99",
+    "url": "/static/media/cartune.052667b9.png"
+  },
+  {
+    "revision": "8473706566358559df44745a20e31191",
+    "url": "/static/media/img.84737065.png"
+  },
+  {
+    "revision": "71466d9851dc44d9a8f3642c9246747d",
+    "url": "/static/media/instagram.71466d98.svg"
+  },
+  {
+    "revision": "a6684b1359242ea5a86881bc85e00a0f",
+    "url": "/static/media/luxcraft.a6684b13.png"
+  },
+  {
+    "revision": "445453423ef6c124025a0a3325022457",
+    "url": "/static/media/meme-me.44545342.png"
+  },
+  {
+    "revision": "3e8264237346e01829ff9e5d92ed51fa",
+    "url": "/static/media/platform2.3e826423.png"
+  },
+  {
+    "revision": "3c1cdee2ac1e9bd06d679eb9d715f0e1",
+    "url": "/static/media/silverdiy.3c1cdee2.png"
+  },
+  {
+    "revision": "2ebfea7ce037aeea219e8fe9c7952c02",
+    "url": "/static/media/website-boombit.2ebfea7c.jpg"
+  },
+  {
+    "revision": "8a22406e0a12dd650512b8c90e24577d",
+    "url": "/static/media/works.8a22406e.svg"
+  }
+]);
